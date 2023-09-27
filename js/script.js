8f6c9f9634d26869ed7fc6c1b582d08e0f8a5dda
Nnellz <Nellie7919@gmail.com>
2023-09-27 14:50:21 -0700

    small changes

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// CONVERT IDENTIFIERS TO CAMEL CASE NOTATION
+// STEP1
+let someMonth;
+function theMonth() {}
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+
+//GIVE EXAMPLE OF NUMERIC LITERAL EXPRESSION
+"64" //GIVE EXAMPLE OF STRING LITERAL EXPRESSION
+`'Hi, Nelly!'` //GIVE EXAMPLE OF BOOLEAN LITERAL EXPRESSION
+`true` //GIVE EXAMPLE OF NULL LITERAL EXPRESSION
+`null`;
+
+//EXAMPLE OF COMPLEX AND VARIABLE EXPRESSION
+let total = 7 + x * 2;
+let isEven = num % 10 === 0;
+
+//DECLARE 9 VARIABLES WITH CAMEL CASING AND HUNGARIAN NOTATION
+strFirstNAME;
+strLastName;
+strAddress;
+strCity;
+strState;
+strZipCode;
+intYourAge;
+strReferralSource;
+boolMayWeContactYou;
+
+//DEMONSTRATE 3 WAYS TO DECLARE AND ASSIGN VALUES TO VARIABLES
+//STEP 1
+let strCity = "New York";
+
+strCity = "New York";
